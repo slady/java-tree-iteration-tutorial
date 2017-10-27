5457f52269bea651f713093013126ff900a9b813
Petr Sládek <petr.sladek@i.cz>
2017-10-27 02:48:49 +0200

    java project name: tree iteration

--- src/main/java/ps/java/tutorial/treeiteration/TreeIteration.java
+++ src/main/java/ps/java/tutorial/treeiteration/TreeIteration.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TreeIterated {
+public class TreeIteration {
 
     public static void main(final String... a) {
         final File root = new File(".");
